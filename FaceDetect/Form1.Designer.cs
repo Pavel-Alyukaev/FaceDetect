b92fb6186f4b4e435612130f3daef00f3a18bd3e
Pavel-Alyukaev <pavel-alyukaev@yandex.ru>
2017-06-17 20:50:49 +0700

    Определение лиц (V001)

--- FaceDetect/Form1.Designer.cs
+++ FaceDetect/Form1.Designer.cs
@@ -29,11 +29,220 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSerchFace = new System.Windows.Forms.Button();
+            this.btnNextFace = new System.Windows.Forms.Button();
+            this.btnPrewFace = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btnAddBase = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnRecognaze = new System.Windows.Forms.Button();
+            this.btnTrein = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox1.Location = new System.Drawing.Point(12, 149);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(400, 300);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox2.Location = new System.Drawing.Point(432, 149);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(200, 200);
+            this.imageBox2.TabIndex = 2;
+            this.imageBox2.TabStop = false;
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(65, 55);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 3;
+            this.btnLoad.Text = "Загрузить";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.BtnLoad_Click);
+            // 
+            // btnSerchFace
+            // 
+            this.btnSerchFace.Location = new System.Drawing.Point(202, 55);
+            this.btnSerchFace.Name = "btnSerchFace";
+            this.btnSerchFace.Size = new System.Drawing.Size(75, 23);
+            this.btnSerchFace.TabIndex = 4;
+            this.btnSerchFace.Text = "Найти";
+            this.btnSerchFace.UseVisualStyleBackColor = true;
+            this.btnSerchFace.Click += new System.EventHandler(this.BtnSerchFace_Click);
+            // 
+            // btnNextFace
+            // 
+            this.btnNextFace.Location = new System.Drawing.Point(557, 355);
+            this.btnNextFace.Name = "btnNextFace";
+            this.btnNextFace.Size = new System.Drawing.Size(75, 23);
+            this.btnNextFace.TabIndex = 5;
+            this.btnNextFace.Text = ">>";
+            this.btnNextFace.UseVisualStyleBackColor = true;
+            this.btnNextFace.Click += new System.EventHandler(this.BtnNextFace_Click);
+            // 
+            // btnPrewFace
+            // 
+            this.btnPrewFace.Location = new System.Drawing.Point(432, 355);
+            this.btnPrewFace.Name = "btnPrewFace";
+            this.btnPrewFace.Size = new System.Drawing.Size(75, 23);
+            this.btnPrewFace.TabIndex = 6;
+            this.btnPrewFace.Text = "<<";
+            this.btnPrewFace.UseVisualStyleBackColor = true;
+            this.btnPrewFace.Click += new System.EventHandler(this.BtnPrewFace_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btnAddBase
+            // 
+            this.btnAddBase.Location = new System.Drawing.Point(501, 426);
+            this.btnAddBase.Name = "btnAddBase";
+            this.btnAddBase.Size = new System.Drawing.Size(75, 23);
+            this.btnAddBase.TabIndex = 8;
+            this.btnAddBase.Text = "Add Base";
+            this.btnAddBase.UseVisualStyleBackColor = true;
+            this.btnAddBase.Click += new System.EventHandler(this.BtnAddBase_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(464, 384);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(168, 20);
+            this.textBox1.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(429, 388);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Имя";
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnRecognaze);
+            this.panel1.Controls.Add(this.btnTrein);
+            this.panel1.Controls.Add(this.btnLoad);
+            this.panel1.Controls.Add(this.btnSerchFace);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(763, 100);
+            this.panel1.TabIndex = 12;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Фото",
+            "Видео"});
+            this.comboBox1.Location = new System.Drawing.Point(19, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 8;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(538, 55);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "label2";
+            // 
+            // btnRecognaze
+            // 
+            this.btnRecognaze.Location = new System.Drawing.Point(408, 55);
+            this.btnRecognaze.Name = "btnRecognaze";
+            this.btnRecognaze.Size = new System.Drawing.Size(75, 23);
+            this.btnRecognaze.TabIndex = 6;
+            this.btnRecognaze.Text = "Распознать";
+            this.btnRecognaze.UseVisualStyleBackColor = true;
+            this.btnRecognaze.Click += new System.EventHandler(this.BtnRecognaze_Click);
+            // 
+            // btnTrein
+            // 
+            this.btnTrein.Location = new System.Drawing.Point(298, 55);
+            this.btnTrein.Name = "btnTrein";
+            this.btnTrein.Size = new System.Drawing.Size(82, 23);
+            this.btnTrein.TabIndex = 5;
+            this.btnTrein.Text = "Тренировать";
+            this.btnTrein.UseVisualStyleBackColor = true;
+            this.btnTrein.Click += new System.EventHandler(this.BtnTrein_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 455);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(400, 89);
+            this.textBox2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(822, 556);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnAddBase);
+            this.Controls.Add(this.btnPrewFace);
+            this.Controls.Add(this.btnNextFace);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSerchFace;
+        private System.Windows.Forms.Button btnNextFace;
+        private System.Windows.Forms.Button btnPrewFace;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btnAddBase;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnTrein;
+        private System.Windows.Forms.Button btnRecognaze;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- FaceDetect/Form1.cs
+++ FaceDetect/Form1.cs
@@ -1,20 +1,261 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Data.SqlClient;
+
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.Face;
 
 namespace FaceDetect
 {
     public partial class Form1 : Form
     {
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Павел\Documents\DBFaces.mdf;Integrated Security=True;Connect Timeout=30");
+
+        private VideoCapture CamVideo = null;
+        private Image<Bgr, byte> TestImage;
+        Image<Gray, byte>[] DetectFace;
+        int[] DetectFaceLabels;
+        List<Rectangle> faces; 
+
+        int CurentFace =-1;
+        public static int WindowsSize = 20;
+        public static Double ScaleIncreaseRate = 1.1;
+        public static int MinNeighbors = 10;
+        CascadeClassifier haar = new CascadeClassifier(@"haarcascade_frontalface_default.xml");
+        
+
         public Form1()
         {
             InitializeComponent();
+            comboBox1.SelectedIndex = 0;
         }
+
+        private void MoveVideo(object sender, EventArgs e)
+        {
+            TestImage = CamVideo.QueryFrame().ToImage<Bgr, byte>().Resize(400,300,Inter.Area);
+            Detect();
+            foreach(Rectangle face in faces)
+                CvInvoke.Rectangle(TestImage, face, new Bgr(Color.Red).MCvScalar, 2);
+            imageBox1.Image = TestImage;
+            faces.Clear();
+        }
+
+
+
+        public void Detect()
+        {// Находим лица
+            Image<Gray, byte> grayframe = TestImage.Convert<Gray, byte>();
+            Rectangle[] facesDetected = haar.DetectMultiScale(
+                   grayframe,
+                   ScaleIncreaseRate,
+                   MinNeighbors,
+                   new Size(WindowsSize, WindowsSize));
+            faces.AddRange(facesDetected);
+        }
+
+
+        //Eigen Parameters
+        int eigenTrainedImageCounter;
+        List<Image<Gray, byte>> eigenTrainingImages = new List<Image<Gray, byte>>();
+        List<int> eigenIntlabels = new List<int>();
+        List<string> eigenlabels = new List<string>();
+        EigenFaceRecognizer eigenFaceRecognizer;
+
+        string FileName(string file)
+        {
+            string[] fileArr = file.Split('\\');
+            var fileName = fileArr[fileArr.Length - 1].Split('_')[0];
+            return fileName;
+        }
+
+        private void BtnLoad_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.SelectedIndex == 0)
+            {
+                //Загружаем JPG файл
+                if (CamVideo != null)
+                {
+                    CamVideo.Stop();
+                    Application.Idle -= MoveVideo;
+                    CamVideo = null;
+                }
+                OpenFileDialog openFileDialog1 = new OpenFileDialog()
+                {
+                    Filter = "Cursor Files|*.jpg",
+                    Title = "Select a JPG File"
+                };
+                if (openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    TestImage = new Image<Bgr, byte>(openFileDialog1.FileName).Resize(400, 300, Inter.Area);
+                    imageBox1.Image = TestImage;
+                }
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                if (CamVideo == null)
+                {
+                    faces = new List<Rectangle>();
+                    CamVideo = new VideoCapture();
+                    Application.Idle += MoveVideo;
+                }
+            }
+        }
+
+        private void BtnSerchFace_Click(object sender, EventArgs e)
+        {
+            Image<Gray, byte> imageFace = TestImage.Clone().Convert<Gray, byte>();
+            faces = new List<Rectangle>();
+            Detect();
+            DetectFace = new Image<Gray, byte>[faces.Count];// = new Image<Bgr, byte>(200, 200);
+            DetectFaceLabels = new int[faces.Count];
+            int i = 0;
+            foreach (Rectangle face in faces)
+            {
+                imageFace.ROI = face;
+                DetectFace[i] = imageFace.Clone().Resize(200, 200, Inter.Area);
+                DetectFaceLabels[i] = i++;
+            }
+            Image<Bgr, byte> imageFace1 = TestImage.Clone();
+            if (faces.Count > 0)
+            {
+                CurentFace = 0;
+                imageBox2.Image = DetectFace[CurentFace];
+                CvInvoke.Rectangle(imageFace1, faces.ElementAt<Rectangle>(CurentFace), new Bgr(Color.Red).MCvScalar, 2);
+            }
+            else
+            {
+                CurentFace = -1;
+            }
+            imageBox1.Image = imageFace1;
+
+        }
+
+        private void BtnTrein_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string dataDirectory = Directory.GetCurrentDirectory() + "\\TrainedFaces";
+
+                string[] files = Directory.GetFiles(dataDirectory, "*.jpg", SearchOption.AllDirectories);
+                eigenTrainedImageCounter = 0;
+                foreach (var file in files)
+                {
+                    Image<Bgr , Byte> TrainedImage = new Image<Bgr, Byte>(file);
+                    TrainedImage._EqualizeHist();
+                    eigenTrainingImages.Add(TrainedImage.Convert<Gray, Byte>());
+                    eigenlabels.Add(FileName(file));
+                    eigenIntlabels.Add(eigenTrainedImageCounter);
+                    eigenTrainedImageCounter++;
+                    textBox2.Text += FileName(file) + "\r\n";
+                }
+                eigenFaceRecognizer = new EigenFaceRecognizer(eigenTrainedImageCounter, 5);
+                eigenFaceRecognizer.Train(eigenTrainingImages.ToArray(), eigenIntlabels.ToArray());
+                //eigenFaceRecognizer.Save(dataDirectory + "\\trainedDataEigen.dat");
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                MessageBox.Show("Nothing in binary database, please add at least a face(Simply train the prototype with the Add Face Button).", "Triained faces load", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+
+        }
+
+
+        private void BtnNextFace_Click(object sender, EventArgs e)
+        {
+            Image<Bgr, byte> imageFace = TestImage.Clone();
+            if (CurentFace < DetectFace.Length - 1)
+            {
+                CurentFace++;
+                imageBox2.Image = DetectFace[CurentFace];
+                CvInvoke.Rectangle(imageFace, faces.ElementAt(CurentFace), new Bgr(Color.Red).MCvScalar, 2);
+                imageBox1.Image = imageFace;
+            }
+            else
+            {
+                MessageBox.Show("Достигнут конец выборки");
+            }
+
+        }
+
+        private void BtnPrewFace_Click(object sender, EventArgs e)
+        {
+            Image<Bgr, byte> imageFace = TestImage.Clone();
+            if (CurentFace > 0)
+            {
+                CurentFace--;
+                imageBox2.Image = DetectFace[CurentFace];
+                CvInvoke.Rectangle(imageFace, faces.ElementAt<Rectangle>(CurentFace), new Bgr(Color.Red).MCvScalar, 2);
+                int X = faces.ElementAt<Rectangle>(CurentFace).X;
+                int Y = faces.ElementAt<Rectangle>(CurentFace).Y;
+                imageBox1.Image = imageFace;
+            }
+            else
+            {
+                MessageBox.Show("Достигнут конец выборки");
+            }
+
+        }
+
+        private void BtnAddBase_Click(object sender, EventArgs e)
+        {
+            string fileName = textBox1.Text + "_" + DateTime.Now.Day.ToString() + "-"
+            + DateTime.Now.Month.ToString() + "-" + DateTime.Now.Year.ToString()
+            + "-" + DateTime.Now.Hour.ToString() + "-" + DateTime.Now.Minute.ToString()
+            + "-" + DateTime.Now.Second.ToString() + ".jpg";
+
+            Directory.CreateDirectory("TrainedFaces");
+            DetectFace[CurentFace].Convert<Gray, byte>().ToBitmap().Save("TrainedFaces\\" + fileName);
+
+        }
+
+        private void BtnRecognaze_Click(object sender, EventArgs e)
+        {
+            Image<Bgr, byte> imageFace = TestImage.Clone();
+            int n = DetectFace.Length;
+            label2.Text = n.ToString();
+            CvInvoke.cvResetImageROI(DetectFace[0]);
+            //DetectFace[0]._EqualizeHist();
+
+            var result = eigenFaceRecognizer.Predict(DetectFace[0]);
+            for (int CurentFace = 0; CurentFace < n; CurentFace++)
+            {
+
+                label2.Text = result.Label.ToString();
+                if (result.Label != -1)
+                {
+                    int X = faces.ElementAt<Rectangle>(CurentFace).X;
+                    int Y = faces.ElementAt<Rectangle>(CurentFace).Y;
+                    CvInvoke.PutText(
+                            imageFace,
+                            eigenlabels[result.Label].ToString(),
+                            new System.Drawing.Point(X, Y),
+                            FontFace.HersheyComplex,
+                            1.0,
+                            new Bgr(0, 255, 0).MCvScalar);
+                    imageBox1.Image = imageFace;
+                    label2.Text = eigenlabels[result.Label].ToString();
+                }
+
+            }
+
+
+        }
+
+        //private void trening()
+        //{
+        //    FaceRecognizer im = new EigenFaceRecognizer(1, 10);
+        //    im.Train<Bgr, byte>(DetectFace, DetectFaceLabels);
+        //    im.Predict()
+        //}
     }
+
+
 }
